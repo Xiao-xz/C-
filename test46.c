f30e9c36d15b49fa18dd8f1cabb685dc4966a9b4
Xiao-xz <3255722165@qq.com>
2021-04-05 19:09:44 +0800

    输出函数

---
+++ test46.c
@@ -0,0 +1,20 @@
+void print(int n)
+{
+	int i = 0;
+	for (i = 1; i <= n; i++)
+	{
+		int j = 0;
+		for (j = 1; j <= i; j++)
+		{
+			printf("%d*%d=%-2d ", i, j, i*j);
+		}
+		printf("\n");
+	}
+}
+int main()
+{
+	int n = 0;
+	scanf("%d", &n);
+	print(n);
+	return 0;
+}
