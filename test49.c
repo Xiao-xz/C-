e43c8d4a307d563e17a9855656bf5271ad4cc5fa
Xiao-xz <3255722165@qq.com>
2021-04-06 19:27:30 +0800

    计算N的K次方

---
+++ test49.c
@@ -0,0 +1,25 @@
+//计算N的K次方
+double Pow(int n, int k)
+{
+	if (k<0)
+	{
+		return (1.0 / Pow(n, -k));
+	}
+	else if (k == 0)
+	{
+		return 1;
+	}
+	else
+	{
+		return n * Pow(n, k - 1);
+	}
+}
+int main()
+{
+	int n = 0;
+	int k = 0;
+	scanf("%d%d", &n, &k);
+	double ret = Pow(n, k);
+	printf("ret = %lf", ret);
+	return 0;
+}
